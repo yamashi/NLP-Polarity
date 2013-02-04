e8980bee294ee0b63cc97b4b1d507a6c2da06590
Yamashi <yamashi@live.com>
2013-02-04 20:30:54 +0100

    - Short message entry

---
+++ Modules/PreprocessingModule/Author.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreprocessingModule
+{
+    class Author
+    {
+        private string name;
+        private string metadata;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Metadata
+        {
+            get { return metadata; }
+            set { metadata = value; }
+        }
+    }
+}
